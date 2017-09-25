e6e152c2d1750dd6ecea790cbd887a463fe4fafb
guitrst <fundan.fundan@gmail.com>
2017-09-25 20:46:57 +0300

    Create hw1.py

---
+++ hw1/hw1.py
@@ -0,0 +1,11 @@
+a = float(input("Введите a: "))
+b = float(input("Введите b: "))
+c = float(input("Введите c: "))
+if a*b==c:
+    print("a умножить на b равно c")
+else:
+    print("a умножить на b не равно c")
+if a/b==c:
+    print("a разделить на b равно c")
+else:
+    print("a разделить на b не равно c")
